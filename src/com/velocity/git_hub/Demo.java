26d1cefd5e577b022a9707e0af11f39f60c97533
vipulborkar <vipulborkar2@gmail.com>
2022-12-24 17:39:09 +0530

    first commit : array list code

---
+++ src/com/velocity/git_hub/Demo.java
@@ -0,0 +1,18 @@
+package com.velocity.git_hub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("vipul");
+		list.add("atul");
+		list.add("prathamesh");
+		list.add("samiksha");
+
+		System.out.println(list);
+	}
+}
